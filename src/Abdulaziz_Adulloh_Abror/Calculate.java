ca6a0267daf7001f8680a9c770f67bc97edd8d85
Aziz1019 <starinc1998@gmail.com>
2022-07-24 10:18:17 +0500

    'First Default Commit'

---
+++ src/Abdulaziz_Adulloh_Abror/Calculate.java
@@ -0,0 +1,8 @@
+package Abdulaziz_Adulloh_Abror;
+
+public class Calculate {
+    //Default method
+    public void multiply(int n, int n2){
+        System.out.println(n * n2);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Abdulaziz_Adulloh_Abror.Calculate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is group work Git Project.");
+        Calculate calculate = new Calculate();
+        calculate.multiply(5, 6);
+    }
+}
